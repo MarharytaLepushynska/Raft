7cb9cc306393dd304ca0136958da669077ff344c
Solomiia Boiko <so.boiko@ukma.edu.ua>
2026-06-13 15:25:47 +0300

    fix: validate reminder time

--- backend/src/main/java/org/naukma/raft/dto/request/ReminderPatchRequest.java
+++ backend/src/main/java/org/naukma/raft/dto/request/ReminderPatchRequest.java
@@ -3,6 +3,7 @@ package org.naukma.raft.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import jakarta.validation.constraints.Future;
 import java.time.LocalDateTime;
 
 @Data
@@ -12,6 +13,7 @@ public class ReminderPatchRequest {
 
     private Long eventId;
 
+    @Future(message = "Reminder time must be in the future")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime reminderTime;
 }
--- backend/src/main/java/org/naukma/raft/dto/request/ReminderRequest.java
+++ backend/src/main/java/org/naukma/raft/dto/request/ReminderRequest.java
@@ -1,6 +1,7 @@
 package org.naukma.raft.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,6 +15,7 @@ public class ReminderRequest {
     private Long eventId;
 
     @NotNull(message = "Reminder time is required")
+    @Future(message = "Reminder time must be in the future")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime reminderTime;
 }
